fa8ae6a6b4d139304d39ae4182237d11658a032e
ezfd8133-sketch <ezfd8133@gmail.com>
2026-03-05 17:41:11 +0100

    java

--- Module26/main.js
+++ Module26/main.js
@@ -1,1 +1,75 @@
-console.log('Helloooo')
+console.log('Helloooo');
+
+var x = 10;
+
+var value = "Digjital School";
+
+var y = 20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+var a = 99;
+
+console.log(a);
+
+a++;
+
+console.log(a);
+
+var b = 201;
+
+b--;
+
+console.log(b);
+
+var c = 500;
+
+c-=54
+
+console.log(c);
+
+var d = 45;
+
+d+=54
+
+console.log(d);
+
+var e = 405;
+
+d*=5
+
+console.log(e);
+
+var f = 670;
+
+f/=10
+
+console.log(f);
+
+var v = 50;
+
+var w = 100;
+
+console.log(w == v);
+
+console.log(w != v);
+
+console.log(w > v);
+
+console.log(w < v);
+
+var emri = "Adolf";
+
+var mbiemri = "Hodza";
+
+var fullName = emri + " " + mbiemri;
+
+console.log(fullName);
+
+console.log(`Hello from ${emri}`);
