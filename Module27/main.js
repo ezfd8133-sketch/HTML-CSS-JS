04c64a8289f30f2394a9331e742e2f86f14de4d6
ezfd8133-sketch <ezfd8133@gmail.com>
2026-03-10 17:42:59 +0100

    78

---
+++ Module27/main.js
@@ -0,0 +1,11 @@
+function displayAlert(){
+    alert("This text is inside the best thing ever created");
+}
+
+displayAlert();
+
+function mbledhja(numri1,numri2){
+    return numri1+numri2;
+}
+
+document.write(mbledhja(5,8));
