e624766f94608972406aedea5e9c3a1b25fcb3d8
ezfd8133-sketch <ezfd8133@gmail.com>
2026-03-17 17:44:55 +0100

    ggs

---
+++ Module28/main.js
@@ -0,0 +1,42 @@
+// var button1 = document.getElementById("btn1");
+
+// button1.onclick = function(){
+//     alert('Hello');
+// }
+
+var button2 = document.querySelector('button');
+
+button2.onclick = function(){
+    alert('Wrong button skill issue');
+}
+
+var button3 = document.getElementById("btn3");
+
+button3.onmouseover = function(){
+    alert('Stop aiming at me gng');
+}
+
+var button4 = document.getElementById("btn4");
+
+button4.onmouseleave = function(){
+    alert('Come back >:(');
+}
+
+var button5 = document.getElementById("btn5");
+
+button5.onmousewheel = function(){
+    alert('Scrolling of me </3');
+}
+
+var button6 = document.getElementById("btn6");
+
+var v_txt = document.getElementById("txt")
+button6.onclick = function(){
+    v_txt.style.color='red';
+}
+
+// ^
+// |
+// |
+
+// Tsundure ahhh buttons
